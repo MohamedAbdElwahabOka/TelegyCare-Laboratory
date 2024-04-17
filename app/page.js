ef712b672e0ca4562bfe8c83ec99baf1896425c5
HagerAshraf248 <aab136001@gmail.com>
2024-04-17 22:29:17 +0200

    "Remove sidebar and payment components, add Login component"
    This commit removes the SendResults, Sidebar, and Payment components from the Home page and adds a new Login component instead. The sidebar and payment components are currently commented out

--- app/page.js
+++ app/page.js
@@ -24,15 +24,7 @@ export default function Home() {
 
   return (
     <>
-      {/* <SendResults/>
-      <Sidebar /> */}
-      {/* <Payment /> */}
-      <div className="flex h-screen">
-        <Sidebar className="w-64 bg-gray-800 text-white px-4 py-8" />
-        <div className="flex-grow bg-gray-100 p-8">
-        </div>
-      </div>
-      <SendResult />
+      <Login/>
     </>
 
   );
