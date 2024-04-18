a1f31fd564ddc55f57b9c23e7d9c5f37486a3a95
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-04-18 03:30:04 +0200

    Update dependencies in package.json and reorganize component imports

--- app/AiResult/page.jsx
+++ app/AiResult/page.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Sidebar from '../_components/Sidebar'
-import AorticSegmentation from '../_components/AorticSegmentation'
+import AorticSegmentation from './_components/AorticSegmentation'
+import MakePDF from './_components/MakePDF'
+import Make from './_components/Make'
+import Makedpfwithjsreport from './_components/Makedpfwithjsreport'
 function Ai_result() {
   return (
     <div>
@@ -8,6 +11,10 @@ function Ai_result() {
       <Sidebar className="w-64 bg-gray-800 text-white px-4 py-8" />
       <div className="flex-grow bg-gray-100 p-8">
         <AorticSegmentation/>
+        {/* <MakePDF/> */}
+         <Make/>
+       {/* <Makedpfwithjsreport/>*/}
+
       </div>
     </div>
     </div>
