e7d5efbfe43f7d44fb39e72d994617fd3f0cf3b7
HagerAshraf248 <aab136001@gmail.com>
2024-04-18 02:33:05 +0200

    "Update Login button to Link component, add search functionality to Table component, update import statement for Testform in patient-details page, and add Sidebar\_for\_PatientD component to patient-details page"

--- app/_components/Login.jsx
+++ app/_components/Login.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
-
+import Link from 'next/link';
 import Image from 'next/image';
 
 const Login = () => {
@@ -61,12 +61,14 @@ const Login = () => {
           </div>
 
           <div>
-            <button
-              type="submit"
-              className="w-full flex justify-center py-3 px-5 border border-transparent rounded-md shadow-sm text-xl font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Sign in
-            </button>
+<Link href="PatientID"
+  
+    type="submit"
+    className="w-full flex justify-center py-3 px-5 border border-transparent rounded-md shadow-sm text-xl font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >
+    Sign in
+
+</Link>
           </div>
         </form>
       </div>
