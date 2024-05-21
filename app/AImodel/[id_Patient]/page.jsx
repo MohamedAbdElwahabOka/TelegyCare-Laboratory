0b861813d8d2fc994ae83893c999b81b2e57cb75
HagerAshraf248 <aab136001@gmail.com>
2024-05-21 18:43:23 +0300

    "Deleted files: Aimodel/page.jsx, AiResult/page.jsx, checkout/page.jsx. Modified files: _components/Sidebar.jsx, patient-details/[patientId]/_components/Sidebar_for_PatientD.jsx."

---
+++ app/AImodel/[id_Patient]/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Sidebar_for_PatientD from '../../patient-details/[patientId]/_components/Sidebar_for_PatientD'
+import FileUploader from '../../_components/FileUploader'
+function Aimodel({params}) {
+  console.log(params.id_Patient)
+  return (
+    <div>
+      <div className="flex h-screen">  
+      <Sidebar_for_PatientD data={params.id_Patient} className="w-64 bg-gray-800 text-white px-4 py-8" />
+      <div className="flex-grow bg-gray-100 p-8">
+        <FileUploader/>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Aimodel
--- app/checkout/[reg_Num_Lab]/page.jsx
+++ app/checkout/[reg_Num_Lab]/page.jsx
@@ -2,25 +2,24 @@
 import React from 'react'
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
-import CheckoutForm from './CheckoutForm.jsx'
-import Sidebar from '../_components/Sidebar'
+import CheckoutForm from '../CheckoutForm'
+// import Sidebar from '../../_components/Sidebar'
+import Sidebar from '../../_components/Sidebar'
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
-function checkout() {
+function checkout({params}) {
     const options = {
 		mode: 'payment',
 		currency: 'usd',
 		amount: 100
 	}
+  // console.log(params?.reg_Num_Lab)
   return (
     <>
-    
-    
-
 
     <div className="flex h-screen">  
-      <Sidebar className="w-64 bg-gray-800 text-white px-4 py-8" />
+      <Sidebar data={params.reg_Num_Lab} className="w-64 bg-gray-800 text-white px-4 py-8" />
       <div className="flex-grow bg-gray-100 p-8">
       <div>
         <Elements stripe={stripePromise} options={options}>
