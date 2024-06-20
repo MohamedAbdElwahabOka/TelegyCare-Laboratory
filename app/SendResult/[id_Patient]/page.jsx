dbe7afbb1ab2a2318d920c304a9a23e06ab3dbe2
HagerAshraf248 <aab136001@gmail.com>
2024-06-20 22:16:05 +0300

    Updated Change to Sidebar Hover and Resolved Undefined Issue

--- app/SendResult/[id_Patient]/page.jsx
+++ app/SendResult/[id_Patient]/page.jsx
@@ -11,7 +11,7 @@ function SendResult({params}) {
   return (
     <div>
      <div className="flex h-screen">  
-      <Sidebar_for_PatientD PatienID={params.id_Patient} labRegNum={labRegNumber} className="w-64 bg-gray-800 text-white px-4 py-8" />
+      <Sidebar_for_PatientD PatientID={params.id_Patient} labRegNum={labRegNumber} className="w-64 bg-gray-800 text-white px-4 py-8" />
       <div className="flex-grow bg-gray-100 p-8">
         <SendResults />
       </div>
